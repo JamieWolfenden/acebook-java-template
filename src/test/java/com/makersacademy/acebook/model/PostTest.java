457bf806fedd7e4d4d0ce7fa116a873cd7cab2a9
Jamie Wolfenden <jamiewolfenden@gmail.com>
2025-07-24 17:33:00 +0100

    Deleted redundant file, allowed viewing profiles from search results

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -5,9 +5,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class PostTest {
 
-	private Post post = new Post("hello");
+	private Post post = new Post("hello", LocalDate.now(), 0);
 
 	@Test
 	public void postHasContent() {
